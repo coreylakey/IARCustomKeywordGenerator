fababe7c149053621866c17a6661e778756078ec
Corey Lakey <corey.lakey@gmail.com>
2017-08-31 14:41:33 -0700

    Included deleting of re-occurrances.
    New file with deleted reoccurances in temp3.txt

--- SyntaxParser/main.c
+++ SyntaxParser/main.c
@@ -2,16 +2,17 @@
 #include "c_file_parser.h"
 #include "ewp_file_parser.h"
 #include "project_parser.h"
+#include "delete_reoccur.h"
 
 int user_filenames_get( char *src_file, char *sfile, char *dir );
 
 int main()
 {
-    char p_src_file[64] = { 0 };
-    char p_dst_file[16] = { "temp1.txt" };
-    char new_file[16] = { "temp2.txt" };
-    char syntax_file[64] = { 0 };
-    char my_dir[128] = { 0 };
+    char p_src_file[64]     = { 0 };
+    char p_dst_file[16]     = { "temp1.txt" };
+    char new_file[16]       = { "temp2.txt" };
+    char syntax_file[64]    = { 0 };
+    char my_dir[128]        = { 0 };
 
     user_filenames_get( p_src_file, syntax_file, my_dir );
 
@@ -36,6 +37,12 @@ int main()
     else
         printf("\r\nParsing Failed\r\n");
 
+    /* Delete re-occurrences. */
+    printf("\r\nDeleting Re-occurrences in syntax file.\r\n");
+    if( delete_reoccur( syntax_file ) )
+        printf("\r\nDeleting Re-occurrences Successful!\r\n");
+    else
+        printf("\r\nDeleting Re-occurrences Failed.\r\n");
 }
 
 int user_filenames_get( char *src_file, char *sfile, char *dir )
